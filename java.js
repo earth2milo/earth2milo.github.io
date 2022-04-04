12aa2cf6e1ac46252776c63b0d7356a7965f57fd
earth2milo <mas10085@nyu.edu>
2022-04-04 14:32:51 -0400

    unable to figure out java

--- java.js
+++ java.js
@@ -1,10 +1,5 @@
-var interval; 
-$("signal").mousedown(function() {
-    interval= setInterval(performWhileMouseDown, 100);
-}).mouseup(function() {
-    clearInterval(interval);
-});
-function performWhileMouseDown() {
-    $("#static").append("document.getElementById(static).style.opacity = 1")
+function myFunction()
+{
+    document.getElementById("textflow").innerHTML="click again";
 }
 
