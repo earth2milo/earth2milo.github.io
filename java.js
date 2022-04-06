3613a5eeb4fd28ef92dd206bdb8ec32dd569ce74
earth2milo <mas10085@nyu.edu>
2022-04-06 13:25:06 -0400

    first time getting javascript to work

--- java.js
+++ java.js
@@ -1,5 +1,31 @@
 function myFunction()
 {
-    document.getElementById("textflow").innerHTML="click again";
+    document.getElementById("textflow").createElement("p");
 }
 
+var btn = document.createElement("button");
+btn.innerHTML = "PRESS TO START";
+btn.display = "flex";
+btn.style.position="absolute";
+btn.style.top = "35%";
+btn.style.right ="45%";
+document.body.appendChild(btn);
+
+
+var text = document.getElementById("wow"); 
+text.addEventListener("mouseover", function() {
+    btn.style.backgroundColor = purple; 
+    btn.style.border = "4px solid #301934";
+
+});
+
+btn.addEventListener("click", function() {
+    wow.style.display="flex";
+   
+
+});
+
+
+function hide() {
+    wow.style.display="flex";
+}
